3f5c47db0184f326fbfcf25f184c572f108f3aa7
dai316 <179296165@qq.com>
2022-02-17 16:23:56 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+<div>
+  {{ val }}
+  <input type="text" v-model="val">
+  <button @click="addTodo">添加</button>
+  <ul>
+    <li v-for="todo in todos ">
+      {{todo}}
+    </li>
+  </ul>
+</div>
+</template>
+<script lang="ts" setup>
+  import {ref} from 'vue'
+  let val =ref('')
+  let todos = ref(['吃饭1','睡觉','学习'])
+  function addTodo () {
+    todos.value.push(val.value)
+    val.value = ''
+  }
+// export  default  {
+  //
+  // setup(){
+  //   let val =ref('')
+  //   return {
+  //     val
+  //   }
+  // }
+// }
+// export default {
+//   data(){
+//     return {
+//       count:1
+//     }
+//   },
+//   methods:{
+//     add(){
+//       this.count++
+//     }
+//   }
+// }
+</script>
+
+<style>
+
+</style>
